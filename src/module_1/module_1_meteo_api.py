982ee57c8f56e2b9501b22d8c4f136c01ccfd489
Ricardo Serrano Soria <101985136+ricardo-serrano@users.noreply.github.com>
2024-09-28 00:10:55 +0200

    Initial code

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -1,7 +1,16 @@
-""" This is a dummy example """
+API_URL = "<https://archive-api.open-meteo.com/v1/archive?>"
+
+COORDINATES = {
+    "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
+    "London": {"latitude": 51.507351, "longitude": -0.127758},
+    "Rio": {"latitude": -22.906847, "longitude": -43.172896},
+}
+VARIABLES = ["temperature_2m_mean", "precipitation_sum", "wind_speed_10m_max"]
+
 
 def main():
     raise NotImplementedError
+
 
 if __name__ == "__main__":
     main()
